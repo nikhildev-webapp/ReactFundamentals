90230a6335a2890a3156d76447e701607f8bd119
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-11-17 19:19:55 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import ExerciseComponent from './components/ExerciseComponent'
 
 
 function App() {
   return (
     <>
-     
+     <ExerciseComponent/>
     </>
   )
 }
---
+++ src/components/ExerciseComponent.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ExerciseComponent = () => {
+  return (
+      <>
+          <div className="exeContainer">
+              <span id='alert'>Don't Touch this component it is fixed</span>
+              <h1>React Fundamental</h1>
+              <p>I practice the react-fundamental</p>
+              <p>By doing the exercise and Mini-project</p>
+              <p>I Build the Different components for Practice Different topics</p>
+              <p>MyMotivation:<span id='myMotivation'>To get Job In 2026</span></p>
+          </div>
+      </>
+  )
+}
+
+export default ExerciseComponent
