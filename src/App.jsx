f259bb998055be5d1d7a47ff77d200cfdf3dcf41
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-11-19 19:01:19 +0530

    clean unnecessary import value

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import './App.css'
-import CharacterCounter from './components/CharacterCounter'
 import ExerciseComponent from './components/ExerciseComponent'
-import InputTracker from './components/InputTracker'
-import LoginForm from './components/LoginForm'
-
-
 
 function App() {
   return (
     <>
       <ExerciseComponent />
       {/* Add your exercise component here⬇️*/}
-      
+
     </>
   )
 }
