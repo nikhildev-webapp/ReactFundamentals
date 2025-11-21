32edde2cdbbd47307b85afc0704f13ebae1bf103
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-11-21 18:44:38 +0530

    remove unnecessary elemnt from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import './App.css'
-import Contact from './components/Contact/Contact'
 import ExerciseComponent from './components/ExerciseComponent'
-import Form from './components/Form/Form'
+
 
 function App() {
   return (
     <>
       <ExerciseComponent />
       {/* Add your exercise component here⬇️*/}
-      <Form/>
+      
     </>
   )
 }
