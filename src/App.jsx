fb296f41226ec6a5060305aebdadffcd763b3479
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-11-21 17:39:10 +0530

    create form exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import Contact from './components/Contact/Contact'
 import ExerciseComponent from './components/ExerciseComponent'
+import Form from './components/Form/Form'
 
 function App() {
   return (
     <>
       <ExerciseComponent />
       {/* Add your exercise component here⬇️*/}
-      
+      <Form/>
     </>
   )
 }
