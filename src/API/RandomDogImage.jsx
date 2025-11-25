807841d476be8266a9dedb0b25897bec055ce22e
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-11-25 15:58:23 +0530

    create-api-project-1-random-dog-image

---
+++ src/API/RandomDogImage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RandomDogImage = () => {
+  return (
+      <>
+          <h1>RandomDogImage</h1>
+          <p>Click the button to fecth Random dog Image</p>
+          <button>GetDogImage</button>
+      </>
+  )
+}
+
+export default RandomDogImage
